4df6dfc2549785d0461bf1340a5ff08758812051
KillWave <35000128+KillWave@users.noreply.github.com>
2020-11-14 18:28:06 +0800

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,3 @@
+import { TemplateResult } from './result'
+export {render} from './render'
+export const html = (strings: TemplateStringsArray, ...values: unknown[]) => new TemplateResult(strings, values);
