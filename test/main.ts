38c5b67f6c07ac501df81e52dcaa76ee122d669a
KillWave <1353207933@qq.com>
2020-11-14 19:14:54 +0800

    fix:frist future-html version 0.0.1

--- test/main.ts
+++ test/main.ts
@@ -1,28 +1,37 @@
-import {html,render} from '../src/index'
+import { html, render } from '../src/index'
 const child = html`
-    <dialog></dialog>
+    <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
 `;
-const h = (text,num)=> html`
-    <div data="${text}" >123${num}
-        ${child}
-        ${document.createTextNode("text Node")}
+const h = (text, num) => html`
+    <div data="${text}">123${num}
+      ${child}
+      ${document.createTextNode("text Node")}
     </div>
 `
 const root = document.querySelector('#root')
 var observer = new MutationObserver(function (mutations, observer) {
-    mutations.forEach(function (mutation) {
-      console.log(mutation, 11);
-    });
+  mutations.forEach(function (mutation) {
+    console.log(mutation, 11);
   });
-  observer.observe(root, {
-    attributes: true,
-    characterData: true,
-    childList: true,
-    subtree: true,
-    attributeOldValue: true,
-    characterDataOldValue: true,
-  });
-render(h('text1',1),root)
-setTimeout(()=>{
-    render(h('text2',2),root)
-},3000);
+});
+observer.observe(root, {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+  attributeOldValue: true,
+  characterDataOldValue: true,
+});
+render(h('text1', 1), root)
+// setTimeout(() => {
+//   render(h('text2', 2), root)
+// }, 3000);
