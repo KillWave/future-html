128408402963f66782e6bf17837eaa06c3ad810d
KillWave <1353207933@qq.com>
2020-11-16 16:18:38 +0800

    fix:init version 0.1.11

--- test/main.ts
+++ test/main.ts
@@ -1,4 +1,9 @@
-import { html, render } from '../src/index'
+import { html, render,destroy } from '../src/index'
+
+//html 字符串模板
+//用法  const j =  html`<div>${变量}</div>`
+//render(j,你需要添加的根元素)
+//destory(你需要添加的根元素) 不需要时候释放内存
 const h = (text, num,array) => html`
     <div data="${text}">123${num}
       ${html `<div>${num}</div>`}
@@ -9,21 +14,10 @@ const h = (text, num,array) => html`
       })}
     </div>
 `
-const root = document.querySelector('#root')
-var observer = new MutationObserver(function (mutations, observer) {
-  mutations.forEach(function (mutation) {
-    console.log(mutation, 11);
-  });
-});
-observer.observe(root, {
-  attributes: true,
-  characterData: true,
-  childList: true,
-  subtree: true,
-  attributeOldValue: true,
-  characterDataOldValue: true,
-});
+const root = document.querySelector('#root');
+
 render(h('text1', 1,[5,6,7,8,9,10]  ), root)
 setTimeout(() => {
-  render(h('text2', 2,[1,2,3,4]), root)
+  render(h('text2', 2,[1,2,3,4]), root);
+  destroy(root);
 }, 3000);
