fe0954abc047b786235cc16ddc310f12b54924f7
KillWave <1353207933@qq.com>
2020-11-18 21:01:21 +0800

    fix:performance lib  version 0.0.2

--- lib/src/index.d.ts
+++ lib/src/index.d.ts
@@ -1,3 +1,4 @@
 import { TemplateResult } from './result';
 export * from './interfaces';
 export declare const html: (strings: TemplateStringsArray, ...values: unknown[]) => TemplateResult;
+export { render, destroy } from './render';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import { TemplateResult } from './result'
 export * from './interfaces'
 export const html = (strings: TemplateStringsArray, ...values: unknown[]) => new TemplateResult(strings, values);
+export {render,destroy} from './render'
